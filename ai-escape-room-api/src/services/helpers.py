4db719410877e579e6d0ff6000a81260c822c16d
Bacsur Martin <herrbacsm@gmail.com>
2025-11-07 14:49:49 +0100

    feat: return camelCase keys in json response

--- ai-escape-room-api/src/services/helpers.py
+++ ai-escape-room-api/src/services/helpers.py
@@ -3,12 +3,12 @@ from dataclasses import asdict, is_dataclass
 
 def dataclass_to_dict(obj):
     if is_dataclass(obj):
-        return {k: dataclass_to_dict(v) for k, v in asdict(obj).items()}
+        return {k[:1].lower() + k[1:]: dataclass_to_dict(v) for k, v in asdict(obj).items()}
     elif isinstance(obj, uuid.UUID):
         return str(obj)
     elif isinstance(obj, list):
         return [dataclass_to_dict(i) for i in obj]
     elif isinstance(obj, dict):
-        return {k: dataclass_to_dict(v) for k, v in obj.items()}
+        return {str(k)[:1].lower() + str(k)[1:]: dataclass_to_dict(v) for k, v in obj.items()}
     else:
         return obj
