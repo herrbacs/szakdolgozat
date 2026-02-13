baa4a056e8a69983a55775fe578261d57382162e
Bacsur Martin <herrbacsm@gmail.com>
2026-02-13 14:15:01 +0100

    generate ui sprites

--- AI/app.py
+++ AI/app.py
@@ -1,15 +1,19 @@
 from fastapi import FastAPI
 from services.level_service import generate_level_json
 from postprocess.normalize_ids import normalize_level_id_structures
+from services.sprite_service import generate_ui_sprites
+from config import PROJECT_ROOT
 
 app = FastAPI()
 
 @app.get("/generate")
 def generate():
-    level_obj, out_dir = generate_level_json()
-    normalized_level, id_map = normalize_level_id_structures(level_obj)
 
-    return {
-        "response": normalized_level,
-        "id_map": id_map
-    }
+    # level_obj, out_dir = generate_level_json()
+    # normalized_level, id_map = normalize_level_id_structures(level_obj)
+    # generate_ui_sprites(out_dir)
+    generate_ui_sprites(out_dir=PROJECT_ROOT)
+    # return {
+    #     "response": normalized_level,
+    #     "id_map": id_map
+    # }
